e69659820535966273b5bad01e679ae9a4975b45
Jônatas Davi Paganini <jonatas.paganini@toptal.com>
2020-01-12 13:58:12 -0300

    Update to Ruby 2.7 and experimenting with pattern matching

--- lib/axt.rb
+++ lib/axt.rb
@@ -2,116 +2,64 @@ require "axt/version"
 
 module Axt
 
-  module_function def ast_from(code)
-    RubyVM::AST.parse(code).children.last
-  end
-
-  class Selector
-    def initialize(code, ast)
-      @code = code
-      @ast = ast
+  # Hack node to deconstruct the AST node and use with pattern matching.
+  RubyVM::AbstractSyntaxTree::Node.class_eval do
+    def name
+      children.first
     end
 
-    def first_line
-      @ast.first_lineno
+    alias node_type type
+
+    def type
+      node_type.to_s.downcase.to_sym
     end
 
-    def last_line
-     @ast.last_lineno
+    attr_reader :source
+
+    alias node_children children
+
+    def children
+      @children ||= node_children
     end
 
-    def first_column
-      @ast.first_column
-    end
-
-    def last_column
-      @ast.last_column
+    def source=(source)
+      @source = source
+      children.grep(self.class).each do |child|
+        child.source = source
+      end
     end
 
     def expression
-      if last_line == first_line
-        @code.lines[last_line-1][first_column...last_column]
+      if first_lineno == last_lineno
+        @source.lines[first_lineno-1][first_column..last_column]
       else
-        (first_line...(last_line+1)).map do |lineno|
-          case lineno
-          when first_line
-            @code.lines[lineno-1][first_column..-1]
-          when last_line
-            @code.lines[lineno-1][0..last_column]
+        (first_lineno..last_lineno).map do |line|
+          code = @source.lines[line-1]
+          if line == first_lineno
+            code[first_column..-1]
+          elsif line == last_lineno
+            code[0..last_column]
           else
-            @code.lines[lineno-1]
+            code
           end
-        end.join
-      end
-    end
-
-    def find_inner_expression _children=children
-      _children.each do |n|
-        if n.first_line > first_line ||
-          n.first_column > first_column
-          return n
-        elsif n.children.any?
-          sub = find_inner_expression(n.children)
-          return sub if sub
-        end
-      end
-      nil
-    end
-
-    def name
-      until_next_expression = find_inner_expression(children)
-      if until_next_expression
-        last_column_index = until_next_expression.first_column - 1
-      else
-        last_column_index = -1
-      end
-      last_column_index -= 1 while @code.lines[first_line-1][last_column_index,1] =~ /[\s=<>]/
-
-      @code.lines[first_line-1][first_column..last_column_index]
-    end
-
-    def node_type
-      @ast.type.gsub(/NODE_|#{@ast.type}/,'').downcase
-    end
-
-    def children
-      @ast.children.compact.map do |child|
-        case child
-        when nil then nil
-        else
-          self.class.new(@code, child)
         end
       end
     end
-
-    def to_sexp indent: 0
-      str = "(#{node_type} #{name}"
-      children.each do |child|
-        str << "\n"
-        str <<  " " * indent
-        if child.nil?
-          str << 'nil'
-        else
-          str << child.to_sexp(indent: indent + 1)
-        end
-      end
-      str << ")"
-      str
+    def deconstruct
+      [type, children]
+    end
+    def deconstruct_keys keys
+      {type: type, children: children}
     end
   end
 
-  class Parser
-    def initialize(code)
-      @code = code
-      @ast = Axt.ast_from code
+  module_function def ast(code)
+    ast = RubyVM::AbstractSyntaxTree.parse(code)
+    case ast in {type: :scope, children: [*_, ast]}
+    else
+      ast
     end
-
-    def selector
-      Selector.new(@code, @ast)
-    end
-
-    def to_sexp
-      selector.to_sexp
-    end
+    ast.source = code
+    ast
   end
 end
--- spec/axt_spec.rb
+++ spec/axt_spec.rb
@@ -1,48 +1,23 @@
 RSpec.describe Axt do
-  let(:example1) { Axt::Parser.new('answer = 42') }
-  let(:example2) { Axt::Parser.new(code) }
-  let(:code) { "def answer\n42\nend" }
-  describe 'Parser#selector' do
+  describe '.ast' do
     context 'one line parsing' do
-      subject { example1.selector }
-
-      it { is_expected.to be_respond_to(:expression) }
+      subject { Axt.ast('answer = 42') }
       it { expect(subject.expression).to eq('answer = 42') }
-      it { expect(subject.name).to eq('answer') }
-      it { expect(subject.node_type).to eq('dasgn_curr') }
+      it { expect(subject.name).to eq(:answer) }
+      it { expect(subject.type).to eq(:lasgn) }
       it { expect(subject.children.last.expression).to eq('42') }
     end
-
-    context 'multiple lines parsing' do
-      subject { example2.selector }
-
-      it { expect(subject.expression).to eq(code) }
-      it { expect(subject.name).to eq('def answer') }
-      it { expect(subject.node_type).to eq('defn') }
-      it { expect(subject.children.last.children.last.expression).to eq('42') }
-    end
-  end
-
-  describe '#to_sexp' do
-    context 'one line parsing' do
-      subject { example1.to_sexp }
-      it { is_expected.to eq(<<~SEXP.chomp) }
-       (dasgn_curr answer
-       (lit 42))
-      SEXP
-    end
-
     context 'multiline parsing' do
-      subject { example2.to_sexp }
-      specify do
-        is_expected.to eq(<<~SEXP.chomp)
-          (defn def answer
-          (scope def answer
-           (args )
-           (lit 42)))
-          SEXP
-      end
+      subject { Axt.ast(<<~RUBY) }
+        def answer
+           { result: 42 }
+        end
+      RUBY
+      it { expect(subject.children[1].children[-1].expression).to eq(<<~RUBY) }
+        { result: 42 }
+      RUBY
+      it { expect(subject.name).to eq(:answer) }
+      it { expect(subject.children[1].children[-1].name.name.name).to eq(:result) }
     end
   end
-
 end
